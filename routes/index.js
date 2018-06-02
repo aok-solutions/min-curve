5b577f7d9121ade93d63e8241a27f60b88ba3ef6
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2018-06-02 18:48:10 -0500

    display contents of file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable'),
+    fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Upload MMS File' });
+  res.render('index', { title: 'Calculate Minimum Curvature' });
+});
+
+router.post('/fileupload', function(req, res, next) {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+    res.writeHead(200, {'content-type': 'text/plain'});
+    fs.readFile(files.file.path, function (err, data) {
+      res.end(data);
+    })
+  })
 });
 
 module.exports = router;
