a6849b8f154a6a02b67ff310522da082e21a8523
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2018-06-03 03:24:01 -0500

    extract transforms to module

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ let formidable = require('formidable'),
     fs = require('fs'),
     converter = require('json-2-csv');
 
-let calculator = require('./calculator.js')
+let calculate = require('./calculator.js');
+let transform = require('./transformer.js');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Calculate Minimum Curvature' });
@@ -23,103 +24,12 @@ router.post('/fileupload', function(req, res, next) {
           .map(entry => entry.trim())
           .filter(entry => entry.match(regex))
 
-      let deviation =
-        entries.map(entry => {
-          // let degrees = parseInt(entry.slice(5,7)),
-          //     minutes = parseInt(entry.slice(7,9)),
-          //     seconds = parseInt(entry.slice(9,11)),
-          //     incline = degrees + minutes/60 + seconds/3600
-
-          let incline = parseInt(entry.slice(5,8))/100
-
-          return {
-            "depth": entry.slice(0,5),
-            "incline": incline.toFixed(2),
-            "azimuth": entry.slice(8)
-          }
-        })
-
-      let dogLeg =
-        deviation.map((entry, index) => {
-          let prevEntry = deviation[index-1]
-          if (typeof prevEntry != 'undefined') {
-            let dogLegAngle =
-              calculator.dogLegAngle(
-                calculator.toRadians(parseFloat(prevEntry.incline)),
-                calculator.toRadians(parseFloat(entry.incline)),
-                calculator.toRadians(parseFloat(prevEntry.azimuth)),
-                calculator.toRadians(parseFloat(entry.azimuth))
-              )
-
-            return {
-              ...entry,
-              "dogLegAngleRadians": dogLegAngle,
-              "dogLegAngleDegrees": calculator.toDegrees(dogLegAngle)
-            }
-          } else {
-            return {
-              ...entry,
-              "dogLegAngleRadians": 0,
-              "dogLegAngleDegrees": 0
-            }
-          }
-        })
-
-      let ratioFactor =
-        dogLeg.map(entry => {
-          return {
-            ...entry,
-            "ratioFactor": calculator.ratioFactor(entry.dogLegAngleRadians)
-          }
-        })
-
       let calculations =
-        ratioFactor.map((entry, index) => {
-          let prevEntry = deviation[index-1]
-          if (typeof prevEntry != 'undefined') {
-            let north = calculator.north(
-              parseFloat(prevEntry.depth),
-              parseFloat(entry.depth),
-              calculator.toRadians(parseFloat(prevEntry.incline)),
-              calculator.toRadians(parseFloat(entry.incline)),
-              calculator.toRadians(parseFloat(prevEntry.azimuth)),
-              calculator.toRadians(parseFloat(entry.azimuth)),
-              parseFloat(entry.ratioFactor)
-            )
-
-            let east = calculator.east(
-              parseFloat(prevEntry.depth),
-              parseFloat(entry.depth),
-              calculator.toRadians(parseFloat(prevEntry.incline)),
-              calculator.toRadians(parseFloat(entry.incline)),
-              calculator.toRadians(parseFloat(prevEntry.azimuth)),
-              calculator.toRadians(parseFloat(entry.azimuth)),
-              parseFloat(entry.ratioFactor)
-            )
-
-            let vertical = calculator.vertical(
-              parseFloat(prevEntry.depth),
-              parseFloat(entry.depth),
-              calculator.toRadians(parseFloat(prevEntry.incline)),
-              calculator.toRadians(parseFloat(entry.incline)),
-              parseFloat(entry.ratioFactor)
-            )
-
-            return {
-              ...entry,
-              "north": north,
-              "east": east,
-              "vertical": vertical
-            }
-          } else {
-            return {
-              ...entry,
-              "north": 0,
-              "east": 0,
-              "vertical": 0
-            }
-          }
-        })
+        entries
+          .map(transform.deviation)
+          .map(transform.dogLegAngle)
+          .map(transform.ratioFactor)
+          .map(transform.curveCalculations)
 
       converter.json2csv(calculations, (err, csv) => {
         if (err) throw err;
---
+++ routes/transformer.js
@@ -0,0 +1,97 @@
+let calculate = require('./calculator.js');
+
+module.exports = {
+  deviation : (entry) => {
+    // let degrees = parseInt(entry.slice(5,7)),
+    //     minutes = parseInt(entry.slice(7,9)),
+    //     seconds = parseInt(entry.slice(9,11)),
+    //     incline = degrees + minutes/60 + seconds/3600
+
+    let incline = parseInt(entry.slice(5,8))/100
+
+    return {
+      "depth": entry.slice(0,5),
+      "incline": incline.toFixed(2),
+      "azimuth": entry.slice(8)
+    }
+  },
+
+  dogLegAngle : (entry, index, data) => {
+    let prevEntry = data[index-1]
+    if (typeof prevEntry != 'undefined') {
+      let dogLegAngle =
+        calculate.dogLegAngle(
+          calculate.toRadians(parseFloat(prevEntry.incline)),
+          calculate.toRadians(parseFloat(entry.incline)),
+          calculate.toRadians(parseFloat(prevEntry.azimuth)),
+          calculate.toRadians(parseFloat(entry.azimuth))
+        )
+
+      return {
+        ...entry,
+        "dogLegAngleRadians": dogLegAngle,
+        "dogLegAngleDegrees": calculate.toDegrees(dogLegAngle)
+      }
+    } else {
+      return {
+        ...entry,
+        "dogLegAngleRadians": 0,
+        "dogLegAngleDegrees": 0
+      }
+    }
+  },
+
+  ratioFactor : (entry) => {
+    return {
+      ...entry,
+      "ratioFactor": calculate.ratioFactor(entry.dogLegAngleRadians)
+    }
+  },
+
+  curveCalculations : (entry, index, data) => {
+    let prevEntry = data[index-1]
+    if (typeof prevEntry != 'undefined') {
+      let north = calculate.north(
+        parseFloat(prevEntry.depth),
+        parseFloat(entry.depth),
+        calculate.toRadians(parseFloat(prevEntry.incline)),
+        calculate.toRadians(parseFloat(entry.incline)),
+        calculate.toRadians(parseFloat(prevEntry.azimuth)),
+        calculate.toRadians(parseFloat(entry.azimuth)),
+        parseFloat(entry.ratioFactor)
+      )
+
+      let east = calculate.east(
+        parseFloat(prevEntry.depth),
+        parseFloat(entry.depth),
+        calculate.toRadians(parseFloat(prevEntry.incline)),
+        calculate.toRadians(parseFloat(entry.incline)),
+        calculate.toRadians(parseFloat(prevEntry.azimuth)),
+        calculate.toRadians(parseFloat(entry.azimuth)),
+        parseFloat(entry.ratioFactor)
+      )
+
+      let vertical = calculate.vertical(
+        parseFloat(prevEntry.depth),
+        parseFloat(entry.depth),
+        calculate.toRadians(parseFloat(prevEntry.incline)),
+        calculate.toRadians(parseFloat(entry.incline)),
+        parseFloat(entry.ratioFactor)
+      )
+
+      return {
+        ...entry,
+        "north": north,
+        "east": east,
+        "vertical": vertical
+      }
+    } else {
+      return {
+        ...entry,
+        "north": 0,
+        "east": 0,
+        "vertical": 0
+      }
+    }
+  }
+}
