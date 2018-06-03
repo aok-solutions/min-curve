444d02fd662d028e70fe48521d2c135c810df6f1
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2018-06-03 01:17:05 -0500

    calculate dog leg angle (modified input file w/calculated incline)

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,8 @@ router.post('/fileupload', function(req, res, next) {
   form.parse(req, function (err, fields, files) {
     res.writeHead(200, {'content-type': 'text/plain'});
     fs.readFile(files.file.path, function (err, data) {
-      const regex = /([0-9]{14}.[0-9]{2})/
+      // const regex = /([0-9]{14}.[0-9]{2})/
+      const regex = /([0-9]{11}.[0-9]{2})/
       let entries =
         data
           .toString()
@@ -23,21 +24,55 @@ router.post('/fileupload', function(req, res, next) {
 
       let deviation =
         entries.map(entry => {
-          let degrees = parseInt(entry.slice(5,7)),
-              minutes = parseInt(entry.slice(7,9)),
-              seconds = parseInt(entry.slice(9,11)),
-              inclination = degrees + minutes/60 + seconds/3600
+          // let degrees = parseInt(entry.slice(5,7)),
+          //     minutes = parseInt(entry.slice(7,9)),
+          //     seconds = parseInt(entry.slice(9,11)),
+          //     incline = degrees + minutes/60 + seconds/3600
+
+          let incline = parseInt(entry.slice(5,8))/100
 
           return {
             "measured_depth": entry.slice(0,5),
-            "inclination": inclination.toFixed(2),
-            "azimuth": entry.slice(11)
+            "incline": incline.toFixed(2),
+            "azimuth": entry.slice(8)
           }
         })
 
-      res.end(deviation.map(entry => JSON.stringify(entry)).join("\n"));
+      let calculations =
+        deviation.map((entry, index) => {
+          let prevEntry = deviation[index-1]
+          if (typeof prevEntry != 'undefined') {
+            let dogLegAngle =
+              calculateDogLegAngle(
+                parseFloat(prevEntry.incline),
+                parseFloat(entry.incline),
+                parseFloat(prevEntry.azimuth),
+                parseFloat(entry.azimuth)
+              )
+
+            return {
+              ...entry,
+              "dogLegAngleRadians": dogLegAngle,
+              "dogLegAngleDegrees": toDegrees(dogLegAngle)
+            }
+          } else {
+            return {
+              ...entry,
+              "dogLegAngleRadians": 0,
+              "dogLegAngleDegrees": 0
+            }
+          }
+        })
+
+      res.end(calculations.map(entry => JSON.stringify(entry)).join("\n"));
     })
   })
 });
+
+const toRadians = (angle) => angle * (Math.PI / 180)
+const toDegrees = (angle) => angle * (180 / Math.PI)
+const calculateDogLegAngle = (upperIncline, lowerIncline, upperAzimuth, lowerAzimuth) => {
+  return Math.acos(Math.cos(toRadians(lowerIncline - upperIncline)) - (Math.sin(toRadians(upperIncline)) * Math.sin(toRadians(lowerIncline)) * (1 - Math.cos(toRadians(lowerAzimuth - upperAzimuth)))))
+}
 
 module.exports = router;
